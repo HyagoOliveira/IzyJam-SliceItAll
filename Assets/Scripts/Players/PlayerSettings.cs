0186c34af64db65d06023fad1e7b676489a70f7f
Hyago Oliveira <hyagogow@gmail.com>
2023-06-06 16:23:53 -0300

    Add OnPlayerInitialized event

--- Assets/Scripts/Players/PlayerSettings.cs
+++ Assets/Scripts/Players/PlayerSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Izyplay.SliceItAll.Players
@@ -9,8 +10,21 @@ namespace Izyplay.SliceItAll.Players
         [SerializeField] private float torqueForce = 25f;
         [SerializeField] private Vector3 jumpDirection = new Vector3(0f, 1f, 1F);
 
+        /// <summary>
+        /// Action fired when the Player is initialized.
+        /// </summary>
+        public event Action<Player> OnPlayerInitialized;
+
+        public Player Player { get; private set; }
+
         public Vector3 GetTorqueVelocity() => Vector3.right * torqueForce;
         public Vector3 GetJumpVelocity() => jumpDirection.normalized * jumpForce;
+
+        internal void Initialize(Player player)
+        {
+            Player = player;
+            OnPlayerInitialized?.Invoke(Player);
+        }
 
         //TODO: add function to set fields using Remote Config.
     }

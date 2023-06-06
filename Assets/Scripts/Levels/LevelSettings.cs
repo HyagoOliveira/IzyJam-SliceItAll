a83891037348afba51b136b4d3f83aee02f27379
Hyago Oliveira <hyagogow@gmail.com>
2023-06-06 18:41:41 -0300

    Initialize Score when level starts

--- Assets/Scripts/Levels/LevelSettings.cs
+++ Assets/Scripts/Levels/LevelSettings.cs
@@ -6,10 +6,17 @@ namespace Izyplay.SliceItAll.Levels
     [CreateAssetMenu(fileName = "LevelSettings", menuName = "Izyplay/LevelSettings", order = 110)]
     public sealed class LevelSettings : ScriptableObject
     {
+        [SerializeField] private ScoreSettings score;
+
         public event Action OnLevelStarted;
         public event Action OnLevelFinished;
 
-        public void Start() => OnLevelStarted?.Invoke();
+        public void Start()
+        {
+            score.Initialize();
+            OnLevelStarted?.Invoke();
+        }
+
         public void Finish() => OnLevelFinished?.Invoke();
     }
 }

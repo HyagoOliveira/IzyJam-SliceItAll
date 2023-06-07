f44597003a54b11c3c24769258c06f6da6373f7e
Hyago Oliveira <hyagogow@gmail.com>
2023-06-07 10:38:34 -0300

    Add multiplierText field

--- Assets/Scripts/Scores/SlaceableScoreMultiplier.cs
+++ Assets/Scripts/Scores/SlaceableScoreMultiplier.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 namespace Izyplay.SliceItAll.Scores
@@ -6,7 +7,16 @@ namespace Izyplay.SliceItAll.Scores
     public sealed class SlaceableScoreMultiplier : AbstractSlaceableScore
     {
         [SerializeField, Min(0)] private int multiplier = 1;
+        [SerializeField] private TMP_Text multiplierText;
+
+        private void Reset() => multiplierText = GetComponentInChildren<TMP_Text>();
 
         protected override void HandleOnSliced() => settings.Multiply(multiplier);
+
+        private void OnValidate()
+        {
+            if (multiplierText == null) return;
+            multiplierText.text = multiplier.ToString();
+        }
     }
 }

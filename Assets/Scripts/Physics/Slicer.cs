3dab95b3cd5c82d9beacf1191e2f375549a87cbd
Hyago Oliveira <hyagogow@gmail.com>
2023-06-07 19:02:01 -0300

    Remove Audio logic

--- Assets/Scripts/Physics/Slicer.cs
+++ Assets/Scripts/Physics/Slicer.cs
@@ -1,33 +1,20 @@
 using UnityEngine;
-using Izyplay.SliceItAll.Audios;
 
 namespace Izyplay.SliceItAll.Physics
 {
     [DisallowMultipleComponent]
-    [RequireComponent(typeof(AudioSource))]
     public sealed class Slicer : MonoBehaviour
     {
         [SerializeField] private BoxDetector detector;
-        [SerializeField] private AudioClipCollection slicerClips;
-        [SerializeField] private AudioSource audioSource;
 
-        private void Reset()
-        {
-            audioSource = GetComponent<AudioSource>();
-            detector = GetComponentInChildren<BoxDetector>();
-        }
-
+        private void Reset() => detector = GetComponentInChildren<BoxDetector>();
         private void OnEnable() => detector.OnHitChanged += HandleHitChanged;
         private void OnDisable() => detector.OnHitChanged -= HandleHitChanged;
 
         private void HandleHitChanged(Transform hit)
         {
             var isSliceable = hit.TryGetComponent(out ISlaceable slaceable);
-            if (!isSliceable) return;
-
-            slaceable.Slice();
-            var clip = slicerClips.GetRandomClip();
-            audioSource.PlayOneShot(clip);
+            if (isSliceable) slaceable.Slice();
         }
     }
 }

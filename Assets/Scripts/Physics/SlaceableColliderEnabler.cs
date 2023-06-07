2511f2a161fca3cf375d1887ba70e7dcde1fac10
Hyago Oliveira <hyagogow@gmail.com>
2023-06-07 12:15:17 -0300

    Refactor ColliderEnabler -> SlaceableColliderEnabler
    Refactor to disable the local collider when it's sliced

--- Assets/Scripts/Physics/SlaceableColliderEnabler.cs
+++ Assets/Scripts/Physics/SlaceableColliderEnabler.cs
@@ -4,14 +4,22 @@ namespace Izyplay.SliceItAll
 {
     [DisallowMultipleComponent]
     [RequireComponent(typeof(Collider))]
-    public sealed class ColliderEnabler : MonoBehaviour, IEnable
+    public sealed class SlaceableColliderEnabler : MonoBehaviour, IEnable
     {
 #pragma warning disable CS0108 // Member hides inherited member; missing new keyword
         [SerializeField] private Collider collider;
 #pragma warning restore CS0108 // Member hides inherited member; missing new keyword
 
+        private ISlaceable slaceable;
+
         private void Reset() => collider = GetComponent<Collider>();
+        private void Awake() => slaceable = GetComponent<ISlaceable>();
+
+        private void OnEnable() => slaceable.OnSliced += HandleOnSliced;
+        private void OnDisable() => slaceable.OnSliced -= HandleOnSliced;
 
         public void SetEnabled(bool enabled) => collider.enabled = enabled;
+
+        private void HandleOnSliced() => SetEnabled(false);
     }
 }

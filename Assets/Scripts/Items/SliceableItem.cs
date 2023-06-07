8a17cafff9ec1beea3a935348fcfdbfee8385600
Hyago Oliveira <hyagogow@gmail.com>
2023-06-07 12:11:14 -0300

    Remove BoxCollider fields

--- Assets/Scripts/Items/SliceableItem.cs
+++ Assets/Scripts/Items/SliceableItem.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 namespace Izyplay.SliceItAll.Items
 {
     [DisallowMultipleComponent]
-    [RequireComponent(typeof(BoxCollider))]
     public sealed class SliceableItem : MonoBehaviour, ISlaceable
     {
-        [SerializeField] private BoxCollider boxCollider;
         [SerializeField] private GameObject whole;
         [SerializeField] private GameObject sliced;
 
         public event Action OnSliced;
 
-        private void Reset() => boxCollider = GetComponent<BoxCollider>();
-
         public void Slice()
         {
             whole.SetActive(false);
             sliced.SetActive(true);
-            boxCollider.enabled = false;
 
             OnSliced?.Invoke();
         }
--- Assets/Scripts/Items/SliceableScoreSection.cs
+++ Assets/Scripts/Items/SliceableScoreSection.cs
@@ -4,26 +4,18 @@ using UnityEngine;
 namespace Izyplay.SliceItAll.Items
 {
     [DisallowMultipleComponent]
-    [RequireComponent(typeof(BoxCollider))]
     public sealed class SliceableScoreSection : MonoBehaviour, ISlaceable
     {
-        [SerializeField] private BoxCollider boxCollider;
         [SerializeField] private MeshRenderer meshRenderer;
         [SerializeField] private Color slicedColor = Color.red;
 
         public event Action OnSliced;
 
-        private void Reset()
-        {
-            boxCollider = GetComponent<BoxCollider>();
-            meshRenderer = GetComponentInChildren<MeshRenderer>();
-        }
+        private void Reset() => meshRenderer = GetComponentInChildren<MeshRenderer>();
 
         public void Slice()
         {
-            boxCollider.enabled = false;
             meshRenderer.material.color = slicedColor;
-
             OnSliced?.Invoke();
         }
     }
